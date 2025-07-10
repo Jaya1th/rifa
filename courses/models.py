7e3ab58c39d6ff51c8e8e54a7012c46b5b384244
Jayavanth M <64251126+Jaya1th@users.noreply.github.com>
2025-07-10 11:53:22 +0530

    Update models.py

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from .models_openedx import OpenEdxUser, StudentCourseEnrollment
+
 
 class Employee(models.Model):
     employee_id = models.CharField(max_length=20, unique=True)
@@ -13,6 +15,16 @@ class Employee(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.employee_id})"
+
+    def get_openedx_user(self):
+        return OpenEdxUser.objects.using('openedx').filter(email=self.email).first()
+
+    def get_openedx_enrollments(self):
+        user = self.get_openedx_user()
+        if user:
+            return StudentCourseEnrollment.objects.using('openedx').filter(user_id=user.id)
+        return []
+
 
 class Course(models.Model):
     course_id = models.CharField(max_length=100)
@@ -29,3 +41,6 @@ class Enrollment(models.Model):
 
     def __str__(self):
         return f"{self.employee_id} - {self.course_id} - {self.status}"
+    
+
+
